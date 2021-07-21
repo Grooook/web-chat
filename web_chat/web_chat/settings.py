836c529d2cb8d604a68335fed20282ae57f594fe
Grooook <46159526+Grooook@users.noreply.github.com>
2021-07-21 19:11:00 +0200

    add channels

--- web_chat/web_chat/settings.py
+++ web_chat/web_chat/settings.py
@@ -72,8 +72,16 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'web_chat.wsgi.application'
+#WSGI_APPLICATION = 'web_chat.wsgi.application'
 
+ASGI_APPLICATION = 'web_chat.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
